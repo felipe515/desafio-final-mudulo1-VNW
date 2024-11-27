932613120dd82f0382a34e0f53e8cb889485f6ac
Felipe Ferreira <flpferreira1999@gmail.com>
2024-11-27 15:26:21 -0300

    Criando pagina quero doar

--- final/src/pages/queroDoar/QueroDoar.jsx
+++ final/src/pages/queroDoar/QueroDoar.jsx
@@ -2,27 +2,21 @@ import s from './queroDoar.module.scss';
 import logoLivro from '../../assets/Vector.png';
 export default function QueroDoar() {
     return (
-        <section className={s.sectionPrincipal}>
-            <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
+        <section className={s.queroDoarSection}>
+            <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
 
-            <section className={s.sectionForm}>
-
-                <section className={s.sectionTitulo}>
-                    <img src={logoLivro} alt="imagem livro aberto" />
-                    <p>Infromações do Livro</p>
-                </section>
-
-                <section className={s.inputs}>
                     <form action="">
+                        <div>
+                            <img src={logoLivro} alt="imagem livro aberto" />
+                            <h2>Infromações do Livro</h2>
+                        </div>
                         <input type="text" placeholder="Titulo" />
                         <input type="text" placeholder="Categoria" />
                         <input type="text" placeholder="Autor" />
                         <input type="text" placeholder="Link da Imagem" />
-                        <button>Doar</button>
+                        <input className={s.buttonDoar} type="submit" value="Doar" />
                     </form>
-                </section>
 
-            </section>
         </section>
     )
 }
