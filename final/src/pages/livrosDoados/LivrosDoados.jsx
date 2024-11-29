d2fced88483e6c3949059b241b798499ae3271b0
Felipe Ferreira <flpferreira1999@gmail.com>
2024-11-29 15:40:13 -0300

    criando pagina livros doados

--- final/src/pages/livrosDoados/LivrosDoados.jsx
+++ final/src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,15 +1,19 @@
 import s from './livrosDoados.module.scss';
 import livroDoado from '../../assets/livroDoado.png'
-export default function LivrosDoados(){
-    return(
-        <section className={s.sectionPrimeiro}>
+export default function LivrosDoados() {
+    return (
+        <section className={s.livrosDoadoSection}>
             <h2>Livros Doados</h2>
-            
-            <section className={s.livroDoado}>
-                <a href=""><img src={livroDoado} alt="livro doado" /></a>
-                <p>O protagonista</p>
-                <p>Susanne Andrade</p>
-                <p>Ficção</p>
+            <section className={s.containerCards}>
+                <section>
+                    <img src={livroDoado} alt="livro doado" />
+                    <div>
+
+                        <h3>O protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
             </section>
         </section>
     )
